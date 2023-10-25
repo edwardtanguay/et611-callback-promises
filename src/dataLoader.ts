d51157f5ab0a3a2504bb923451bde1f17b48c692
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-26 01:03:22 +0200

    bad sync

--- src/dataLoader.ts
+++ src/dataLoader.ts
@@ -1,5 +1,16 @@
+import axios from 'axios';
 import employees from './data/employees.json';
 
-export const getAsynchronousEmployees = () => {
+export const getSynchronousEmployees = () => {
 	return employees;
 }
+
+export const getAsynchronousCallbackEmployees = () => {
+	const url = 'https://edwardtanguay.vercel.app/share/employees.json';
+	(async () => {
+		const employees = (await axios.get(url)).data
+		console.log('async', employees);
+		return employees;
+	})();
+}
+
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import './style.css'
 import * as dataLoader from './dataLoader';
 
+const asychronousCallbackEmloyees = dataLoader.getAsynchronousCallbackEmployees();
+console.log('main', asychronousCallbackEmloyees);
+
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
 
   <h2>Asynchronous functions with callbacks and promises</h2>
 
-  <h3>Asynchronous employees</h3>
-  ${dataLoader.getAsynchronousEmployees().map(m => m.lastName).join(', ')}
+  <h3>Synchronous employees</h3>
+  ${dataLoader.getSynchronousEmployees().map(m => m.lastName).join(', ')}
+  
 `
