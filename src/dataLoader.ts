de5aaddc9f7b7e526f719a831bf570d130602380
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-26 01:52:12 +0200

    callback solution

--- src/dataLoader.ts
+++ src/dataLoader.ts
@@ -1,16 +1,17 @@
 import axios from 'axios';
 import employees from './data/employees.json';
+import { IEmployee } from './interfaces';
 
 export const getSynchronousEmployees = () => {
 	return employees;
 }
 
-export const getAsynchronousCallbackEmployees = () => {
+export const getAsynchronousCallbackEmployees = (callback: (employees: IEmployee[]) => void) => {
 	const url = 'https://edwardtanguay.vercel.app/share/employees.json';
 	(async () => {
 		const employees = (await axios.get(url)).data
 		console.log('async', employees);
-		return employees;
+		callback(employees);
 	})();
 }
 
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import './style.css'
 import * as dataLoader from './dataLoader';
 
-const asychronousCallbackEmloyees = dataLoader.getAsynchronousCallbackEmployees();
-console.log('main', asychronousCallbackEmloyees);
-
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
+dataLoader.getAsynchronousCallbackEmployees((asynchronousCallbackEmployees) => {
+	document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
 
   <h2>Asynchronous functions with callbacks and promises</h2>
 
   <h3>Synchronous employees</h3>
   ${dataLoader.getSynchronousEmployees().map(m => m.lastName).join(', ')}
-  
+
+  <h3>Asynchronous callback employees</h3>
+  ${asynchronousCallbackEmployees.map(m => m.lastName).join(', ')}
 `
+});
+
