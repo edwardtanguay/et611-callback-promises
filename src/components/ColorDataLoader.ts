6a20a496e36a327171fa081cbbaa1a01bf734631
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-26 00:23:49 +0200

    base

--- src/components/ColorDataLoader.ts
+++
@@ -1,3 +0,0 @@
-export const ColorDataLoader = () => {
-	return ['green', 'yellow', 'red', 'purple'];
-}
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,6 @@
-import { ColorDataLoader } from './components/ColorDataLoader';
-import { EmployeeDataLoader } from './components/EmployeeDataLoader';
-import { IEmployee } from './components/interfaces';
 import './style.css'
 
-const colors = ColorDataLoader();
-
-EmployeeDataLoader((employees: IEmployee[]) => {
-  document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
-<h1>Callbacks and Promises</h1>
-
-<p>There are ${colors.length} colors: ${colors.map(m => `${m}`).join(', ')}</p>
-<p>There are ${employees.length} employees.</p>
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
+<h2>Asynchronous functions with callbacks and promises</h2>
+<p>testing</p>
 `
-});
