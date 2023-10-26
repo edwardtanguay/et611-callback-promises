ddc1aba5d3b9ba20d021d87ebfcac438d32f8e8a
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-26 02:36:47 +0200

    loading ok

--- src/dataLoader.ts
+++ src/dataLoader.ts
@@ -2,16 +2,18 @@ import axios from 'axios';
 import employees from './data/employees.json';
 import { IEmployee } from './interfaces';
 
+const employeesUrl = 'https://edwardtanguay.vercel.app/share/employees.json';
+
 export const getSynchronousEmployees = () => {
 	return employees;
 }
 
-export const getAsynchronousCallbackEmployees = (callback: (employees: IEmployee[]) => void) => {
-	const url = 'https://edwardtanguay.vercel.app/share/employees.json';
-	(async () => {
-		const employees = (await axios.get(url)).data
-		console.log('async', employees);
-		callback(employees);
-	})();
+export const getAsynchronousPromiseEmployees = () => {
+	return new Promise<IEmployee[]>((resolve) => {
+		setTimeout(async () => {
+			const employees: IEmployee[] = (await axios.get(employeesUrl)).data
+			resolve(employees);
+		}, 2000)
+	});
 }
 
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,20 @@
 import './style.css'
 import * as dataLoader from './dataLoader';
 
-dataLoader.getAsynchronousCallbackEmployees((asynchronousCallbackEmployees) => {
-	document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
 
   <h2>Asynchronous functions with callbacks and promises</h2>
 
   <h3>Synchronous employees</h3>
   ${dataLoader.getSynchronousEmployees().map(m => m.lastName).join(', ')}
 
-  <h3>Asynchronous callback employees</h3>
-  ${asynchronousCallbackEmployees.map(m => m.lastName).join(', ')}
+  <h3>Asynchronous promise employees</h3>
+  <div class="asynchronousPromiseEmployees"></div>
 `
-});
 
+const asynchronousPromiseEmployeesElem = document.querySelector<HTMLDivElement>('.asynchronousPromiseEmployees');
+if (asynchronousPromiseEmployeesElem) {
+  asynchronousPromiseEmployeesElem.innerText = 'loading...';
+  const asynchronousPromiseEmployees = await dataLoader.getAsynchronousPromiseEmployees();
+  asynchronousPromiseEmployeesElem.innerText = asynchronousPromiseEmployees.map(m => m.lastName).join(', ');
+}
